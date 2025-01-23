34d81cacae3f412828a3cb12007e435b97eb3845
Aysha M A <aishmish2001@gmail.com>
2025-01-23 14:22:46 +0530

    second commit

--- SpringBootRestEmployeeManagementApp/src/main/java/com/example/demo/exceptions/EmployeeNotFound.java
+++ SpringBootRestEmployeeManagementApp/src/main/java/com/example/demo/exceptions/EmployeeNotFound.java
@@ -1,6 +1,11 @@
 package com.example.demo.exceptions;
 
 public class EmployeeNotFound extends Exception{
+
+	public EmployeeNotFound(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
 	
 
 }
--- SpringBootRestEmployeeManagementApp/src/main/java/com/example/demo/service/EmployeeServiceImpl.java
+++ SpringBootRestEmployeeManagementApp/src/main/java/com/example/demo/service/EmployeeServiceImpl.java
@@ -1,5 +1,24 @@
 package com.example.demo.service;
 
-public class EmployeeServiceImpl implements EmployeeService{
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.example.demo.entity.Employee;
+import com.example.demo.repository.EmployeeRepo;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+	
+	@Autowired
+    EmployeeRepo repository;
+	
+	@Override
+	public String addEmployee(Employee employee) {
+		// TODO Auto-generated method stub
+		Employee emp=repository.save(employee);
+		if(emp!=null) 
+		   return "Employee Saved Successfully";
+		else
+		   return "Failed to Save Employee";
+  }
 }
